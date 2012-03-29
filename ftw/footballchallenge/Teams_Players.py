25592b2f5060012be345071341bed676a89523c6
Timon Tschanz <timon.tschanz@4teamwork.ch>
2012-03-29 14:35:22 +0200

    updated team_players table

---
+++ ftw/footballchallenge/Teams_Players.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from ftw.footballchallenge import Base
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class Teams_Players(Base):
+    __tablename__='teams_players'
+    
+    team_id = Column('team_id', Integer, ForeignKey('teams.id'), primary_key=True),
+    player_id = Column('player_id', Integer, ForeignKey('players.id'), primary_key=True),
+    is_starter = Column('is_starter', Boolean)
+    player = relationship("Player", backref="teams")
+
+
+    def __init__(self, player, is_starter):
+        self.is_starter = is_starter
+        self.player.append(player)
+    
+    
