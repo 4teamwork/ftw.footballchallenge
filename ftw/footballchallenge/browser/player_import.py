a74915ca455b1cad67688fb5935fd3bbc6a8b9e1
Thomas Buchberger <t.buchberger@4teamwork.ch>
2012-04-23 10:59:16 +0200

    Added default urls for player import.

--- ftw/footballchallenge/browser/player_import.py
+++ ftw/footballchallenge/browser/player_import.py
@@ -7,10 +7,32 @@ from ftw.footballchallenge.playerimport import import_team
 
 class PlayerImportSchema(interface.Interface):
 
-    urls = schema.List(title=_(u'label_import_urls', default="Urls"),
-                       value_type = schema.TextLine())
-    event = schema.Choice(title=_('label_import_event', default="Event"),
-                          source=u"EventFactory")
+    urls = schema.List(
+        title=_(u'label_import_urls', default="Urls"),
+        value_type=schema.TextLine(),
+        default=[
+            'http://www.transfermarkt.ch/de/daenemark/startseite/nationalmannschaft_3436.html',
+            'http://www.transfermarkt.ch/de/deutschland/startseite/nationalmannschaft_3262.html',
+            'http://www.transfermarkt.ch/de/england/startseite/nationalmannschaft_3299.html',
+            'http://www.transfermarkt.ch/de/frankreich/startseite/nationalmannschaft_3377.html',
+            'http://www.transfermarkt.ch/de/griechenland/startseite/nationalmannschaft_3378.html',
+            'http://www.transfermarkt.ch/de/irland/startseite/nationalmannschaft_3509.html',
+            'http://www.transfermarkt.ch/de/italien/startseite/nationalmannschaft_3376.html',
+            'http://www.transfermarkt.ch/de/kroatien/startseite/nationalmannschaft_3556.html',
+            'http://www.transfermarkt.ch/de/niederlande/startseite/nationalmannschaft_3379.html',
+            'http://www.transfermarkt.ch/de/polen/startseite/nationalmannschaft_3442.html',
+            'http://www.transfermarkt.ch/de/portugal/startseite/nationalmannschaft_3300.html',
+            'http://www.transfermarkt.ch/de/russland/startseite/nationalmannschaft_3448.html',
+            'http://www.transfermarkt.ch/de/schweden/startseite/nationalmannschaft_3557.html',
+            'http://www.transfermarkt.ch/de/spanien/startseite/nationalmannschaft_3375.html',
+            'http://www.transfermarkt.ch/de/tschechien/startseite/nationalmannschaft_3445.html',
+            'http://www.transfermarkt.ch/de/ukraine/startseite/nationalmannschaft_3699.html',
+        ],
+    )
+    event = schema.Choice(
+        title=_('label_import_event', default="Event"),
+        source=u"EventFactory",
+    )
 
 
 class PlayerImportForm(form.Form):
