09afb4e16987f557e23c85aa5a9d00c1baf4a4d0
Timon Tschanz <timon.tschanz@4teamwork.ch>
2012-04-27 17:01:08 +0200

    added nationcount changed traversing

--- ftw/footballchallenge/browser/team_overview.py
+++ ftw/footballchallenge/browser/team_overview.py
@@ -3,23 +3,27 @@ from z3c.saconfig import named_scoped_session
 from ftw.footballchallenge.Teams_Players import Teams_Players
 from zope.app.component.hooks import getSite 
 from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+from zope.interface import implements
+from zope.publisher.interfaces import IPublishTraverse
 
 
 class TeamOverview(BrowserView):
     """Defines a view for the league which displays the ranking."""
 
+    implements(IPublishTraverse)
     
     template = ViewPageTemplateFile("team_overview.pt")
 
     def __call__(self):
-        self.request['disable_plone.leftcolumn'] = True
-        self.request['disable_plone.rightcolumn'] = True
-        self.context.Title = self.context.name
         return self.template()
-    
+
+    def publishTraverse(self, request, name):
+        self.team_id = name
+        return self
+
     def get_players(self, starters):
         session = named_scoped_session('footballchallenge')
-        teams_players = session.query(Teams_Players).filter_by(team_id=self.context.id_).filter_by(is_starter=starters).all()
+        teams_players = session.query(Teams_Players).filter_by(team_id=self.team_id).filter_by(is_starter=starters).all()
         players = []
         for team_player in teams_players:
             players.append(team_player.player)
@@ -36,3 +40,19 @@ class TeamOverview(BrowserView):
 
     def get_substitutes(self):
         return self.get_players(False)
+
+    def calculate_nations(self):
+        session = named_scoped_session('footballchallenge')
+        teams_players = session.query(Teams_Players).filter(Teams_Players.team_id==self.team_id).all()
+        nations = []
+        sub_nations = []
+        for team_player in teams_players:
+            if team_player.is_starter == True:
+                player = team_player.player
+                if not player.nation_id in nations and not player.nation_id in sub_nations:
+                    nations.append(player.nation_id)
+            else:
+                player = team_player.player
+                if not player.nation_id in nations and not player.nation_id in sub_nations:
+                    sub_nations.append(player.nation_id)
+        return {'starter_nations': len(nations), 'sub_nations': len(sub_nations)+len(nations)}
