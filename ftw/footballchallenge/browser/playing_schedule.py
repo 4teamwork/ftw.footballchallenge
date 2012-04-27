2661ff36d37dc3086289bd268d80e9689791f808
Timon Tschanz <timon.tschanz@4teamwork.ch>
2012-04-27 16:49:27 +0200

    added playing schedule

---
+++ ftw/footballchallenge/browser/playing_schedule.py
@@ -0,0 +1,28 @@
+from zope.publisher.browser import BrowserView
+from z3c.saconfig import named_scoped_session
+from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+import datetime
+from ftw.footballchallenge.event import Event
+from ftw.footballchallenge.game import Game
+
+
+class PlayingScheduleView(BrowserView):
+    """Defines a view for the league which displays the ranking."""
+    
+    template = ViewPageTemplateFile("playing_schedule.pt")
+
+
+    def get_games(self):
+        session = named_scoped_session('footballchallenge')
+        event_id = session.query(Event).filter(Event.LockDate > datetime.date.today()).one().id_
+        games = session.query(Game).filter(Game.events_id == event_id).order_by(Game.date).group_by(Game.stage).all()
+        game_dict = {}
+        for game in games:
+            if game_dict.get(game.stage, None):
+                game_dict[game.stage].append(game)
+            else:
+                game_dict[game.stage] = [game]
+        return game_dict
+
+    def __call__(self):
+        return self.template()
