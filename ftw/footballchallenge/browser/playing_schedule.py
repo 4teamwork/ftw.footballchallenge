732b137958b052d858d7e2f81de9ee8fde929258
Thomas Buchberger <t.buchberger@4teamwork.ch>
2012-04-29 14:13:52 +0200

    Fixed query for schedule

--- ftw/footballchallenge/browser/playing_schedule.py
+++ ftw/footballchallenge/browser/playing_schedule.py
@@ -15,14 +15,14 @@ class PlayingScheduleView(BrowserView):
     def get_games(self):
         session = named_scoped_session('footballchallenge')
         event_id = session.query(Event).filter(Event.LockDate > datetime.date.today()).one().id_
-        games = session.query(Game).filter(Game.events_id == event_id).order_by(Game.date).group_by(Game.round_).all()
-        game_dict = {}
+        games = session.query(Game).filter(Game.events_id == event_id).order_by(Game.date).all()
+        games_per_round = {}
         for game in games:
-            if game_dict.get(game.round_, None):
-                game_dict[game.round_].append(game)
+            if game.round_ in games_per_round:
+                games_per_round[game.round_].append(game)
             else:
-                game_dict[game.round_] = [game]
-        return game_dict
+                games_per_round[game.round_] = [game]
+        return games_per_round
 
     def __call__(self):
         return self.template()
