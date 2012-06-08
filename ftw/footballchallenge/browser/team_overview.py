74fdd7fbbeb601fd62f6658734212fc150f540e1
Timon Tschanz <timon.tschanz@4teamwork.ch>
2012-06-09 01:22:30 +0200

    fix bug where anonymous user couldn't see team_overview

--- ftw/footballchallenge/browser/team_overview.py
+++ ftw/footballchallenge/browser/team_overview.py
@@ -31,13 +31,13 @@ class TeamOverview(BrowserView):
         open_events = session.query(Event).filter(Event.deadline > datetime.datetime.now()).all()
         membershiptool = getToolByName(self.context, 'portal_membership')
         userid = membershiptool.getAuthenticatedMember().getId()
-        if not session.query(Team).filter(Team.user_id == userid).all():
+        if not session.query(Team).filter(Team.user_id == userid).all() and userid:
             msg = _(u'msg_no_team', default="You have currently no Teams. Here you can create one.")
             IStatusMessage(self.request).addStatusMessage(
                 msg, type='info')
             return self.request.response.redirect(self.context.absolute_url()+'/edit_team')
-        team = session.query(Team).filter(Team.user_id == userid).one()
         if not self.team_id:
+            team = session.query(Team).filter(Team.user_id == userid).one()
             self.team_id = int(team.id_)
         if not open_events or team.id_ == int(self.team_id):
             return self.template()
@@ -57,7 +57,11 @@ class TeamOverview(BrowserView):
         players = []
         for team_player in teams_players:
             players.append(team_player.player)
-        return players
+        sorted_players = sorted(players, key=lambda player: player.position)
+        for index, player in enumerate(sorted_players):
+            if player.position == "keeper":
+                sorted_players.insert(0, sorted_players.pop(index))
+        return sorted_players
         
     def generate_link(self, player):
         portal = getSite()
