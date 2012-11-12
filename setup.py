b01b8b6b5059b7b05b2cc8254d89686f3dd282bd
Jonas Baumann <jone@jone.ch>
2012-11-12 14:18:22 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -1,34 +1,37 @@
 from setuptools import setup, find_packages
-import os
 
 version = '1.0'
 maintainer = 'Timon Tschanz'
 
 tests_require = [
     'plone.app.testing',
-    'plone.mocktestcase'
-    '']
+    'plone.mocktestcase',
+    ]
 
 setup(name='ftw.footballchallenge',
       version=version,
       description="Some sort of Footballmanagergame based on Plone",
       # long_description=open('README.rst').read() + '\n' + \
       #   open(os.path.join('docs', 'HISTORY.txt')).read(),
+
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         ],
+
       maintainer=maintainer,
-      keywords='Football Plone Python',
       author='4teamwork GmbH',
-      author_email='info@4teamwork.ch',
-      url='github.com/4teamwork/ftw.footballchallenge',
+      author_email='mailto:info@4teamwork.ch',
+      keywords='Football Plone Python',
+      url='https://github.com/4teamwork/ftw.footballchallenge',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw', ],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
           'setuptools',
           'SQLAlchemy',
@@ -38,11 +41,10 @@ setup(name='ftw.footballchallenge',
           'ftw.tabbedview'
           # -*- Extra requirements: -*-
       ],
-      
+
       tests_require=tests_require,
-      extras_require=dict(tests=tests_require),      
-      
-      
+      extras_require=dict(tests=tests_require),
+
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
