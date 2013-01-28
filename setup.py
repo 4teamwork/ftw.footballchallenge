f48cdbf01f1fe8eddb320fe824131193b7a824b2
Jonas Baumann <jone@jone.ch>
2013-01-28 11:09:50 +0100

    Update classifiers.

--- setup.py
+++ setup.py
@@ -16,9 +16,14 @@ setup(name='ftw.footballchallenge',
           open(os.path.join('docs', 'HISTORY.txt')).read(),
 
       # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+
       classifiers=[
-        "Programming Language :: Python",
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.1',
+        'Framework :: Plone :: 4.2',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
 
       maintainer=maintainer,
