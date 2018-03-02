dd746f0539d188125ecbc940fbe339ab5f1f1e62
Jonas Baumann <jone@jone.ch>
2018-03-02 13:30:41 +0100

    Drop Plone 4.2 support.

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(name='ftw.footballchallenge',
 
       classifiers=[
         'Framework :: Plone',
-        'Framework :: Plone :: 4.2',
         'Framework :: Plone :: 4.3',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
